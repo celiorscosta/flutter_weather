fd06aee40404b68c66e1d660472bce49dca6d1fe
celiorscosta <celiorscosta@hotmail.com>
2021-01-12 12:51:35 -0300

    final tutorial

---
+++ lib/settings/bloc/settings_bloc.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+part 'settings_event.dart';
+part 'settings_state.dart';
+
+class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
+  SettingsBloc()
+      : super(SettingsState(temperatureUnits: TemperatureUnits.celsius));
+
+  @override
+  Stream<SettingsState> mapEventToState(SettingsEvent event) async* {
+    if (event is TemperatureUnitsToggled) {
+      yield SettingsState(
+        temperatureUnits: state.temperatureUnits == TemperatureUnits.celsius
+            ? TemperatureUnits.fahrenheit
+            : TemperatureUnits.celsius,
+      );
+    }
+  }
+}
